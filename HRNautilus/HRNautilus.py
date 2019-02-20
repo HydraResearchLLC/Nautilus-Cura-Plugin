78e6c615012b9277988b74254c33b7ab0bd4a5a2
roseg0ld <44655016+roseg0ld@users.noreply.github.com>
2019-02-20 15:19:01 -0800

    Added acknowledgements

--- HRNautilus/HRNautilus.py
+++ HRNautilus/HRNautilus.py
@@ -5,9 +5,18 @@
 #
 # Written by Zach Rose
 # Based on the Dremel 3D20 plugin written by Tim Schoenmackers
+# Also based on the DuetRRF plugin written by Thomas Kriechbaumer
+# Also includes code from the GCodeWriter Plugin by Ultimaker
 #
 # the Dremel plugin source can be found here:
 # https://github.com/timmehtimmeh/Cura-Dremel-3D20-Plugin
+#
+# the Duet plugin source can be found here:
+# https://github.com/Kriechi/Cura-DuetRRFPlugin
+#
+# the GCodeWriter plugin source can be found here:
+# https://github.com/Ultimaker/Cura/tree/master/plugins/GCodeWriter
+#
 #
 # This plugin is released under the terms of the LGPLv3 or higher.
 # The full text of the LGPLv3 License can be found here:
