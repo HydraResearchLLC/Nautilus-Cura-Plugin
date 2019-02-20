bd866de9cb82462942668205f9bec6b65323e8de
roseg0ld <44655016+roseg0ld@users.noreply.github.com>
2019-02-20 15:21:12 -0800

    Removing unnecessary lines

--- HRNautilus/__init__.py
+++ HRNautilus/__init__.py
@@ -1,17 +1,8 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
-
 from . import HRNautilus
 from . import NautilusDuet
 
-
-from UM.i18n import i18nCatalog
-catalog = i18nCatalog("cura")
-
 def getMetaData():
-    return {
-
-    }
+    return {}
 
 def register(app):
     return {"extension": [
