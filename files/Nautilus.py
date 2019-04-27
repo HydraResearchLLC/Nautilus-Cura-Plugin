c9310e4d474952da5f6525a904a5496cd0a3b134
Zach <zachrose@hydraresearch3d.com>
2019-04-26 17:38:47 -0700

    updated version nos

--- files/Nautilus.py
+++ files/Nautilus.py
@@ -65,7 +65,7 @@ class Nautilus(QObject, MeshWriter, Extension):
     # 1) here
     # 2) plugin.json
     # 3) package.json
-    version = "0.6.1"
+    version = "0.7.0"
 
     ##  Dictionary that defines how characters are escaped when embedded in
     #   g-code.
